762563c2eeae224eeed80b018638a7fa6d2371f1
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 17:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value)  {
-  obj[key] = value;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)  {
+  object[key] = value;
   return obj;
 }
 function deleteFromObjectByKey(object, key) {
