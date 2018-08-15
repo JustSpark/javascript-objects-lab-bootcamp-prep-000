b141329ac8e97ba0c7c289499ec9fd5b9ee068bf
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 17:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = new Object({fudge: 'chocolate'}, {cake: 'flour'});
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
